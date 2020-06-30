51ca2249a66d95675ec31d0e44f40a4205dab010
yoan parede <parede.yoan.amcs@gmail.com>
2020-06-30 16:49:02 +0200

    modif size mobile header

--- assets/php/form_1.php
+++ assets/php/form_1.php
@@ -1,4 +1,7 @@
-<?php	
+<?php
+
+	// Recuperation information prospect
+
 	$name = $_POST['name'];
 	$firstName = $_POST['firstName'];
 	$email = $_POST['email'];
@@ -12,10 +15,12 @@
 	$email_body = "Vous avez recu un nouveaux message \n\n".
 				  "Nom: $name \nEmail: $email \nsujet: $subject \nMessage: $message \n";
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
-	$headers .= "From: contact@yoursite.com\n";
-	$headers .= "Reply-To: $subject";	
+	$headers .= "From: decap-sablage.com\n";
+	$headers .= "Reply-To: $to";	
 	
 	mail($to,$email_subject,$email_body,$headers); // Post message
+	
+		// test que le formulaire soit bien rempli
 	if(empty($_POST['name']) && strlen($_POST['name']) == 0 || empty($_POST['email']) && strlen($_POST['email']) == 0 || empty($_POST['subject']) && strlen($_POST['subject']) == 0 || empty($_POST['message']) && strlen($_POST['message']) == 0)
 	{
 		return false;

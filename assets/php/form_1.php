3d25204bdaccfcb09b17b0d5dccb89e7b1afa557
yoan parede <parede.yoan.amcs@gmail.com>
2020-07-13 18:38:24 +0200

    modif form but not fonctionaly

--- assets/php/form_1.php
+++ assets/php/form_1.php
@@ -8,60 +8,40 @@
         <link href="https://fonts.googleapis.com/css2?family=Comfortaa&display=swap" rel="stylesheet">
     </head>
 	<body>
-		<?php
-			/*echo("creation du mail");
-			mail('parede.yoan.amcs@gmail.com', "sujet test", 'voici le message');
-			$m ="<p>message envoyer</p>";
-			echo($m);
-			// Recuperation information prospect
-			if(isset($_POST['name']) && ($_POST['firstName']) && ($_POST['email']) && ($_POST['subject']) && ($_POST['message'])) {
-				$name = $_POST['name'];
-				$firstName = $_POST['firstName'];
-				$email = $_POST['email'];
-				$subject = $_POST['subject'];
-				$message = $_POST['message'];
+		<div class="content_formPhp">
+			<?php
+				// Recuperation information prospect
+				//if(isset($_POST['Nom']) && ($_POST['Prenom']) && ($_POST['email']) && ($_POST['subject']) && ($_POST['message'])) {
+					echo("<p>creation du mail</p>");
 
-				$to = 'parede.yoan.amcs@gmail.com'; // Email submissions are sent to this email
+					$name = $_POST['Nom'];
+					$firstName = $_POST['Prenom'];
+					$email = $_POST['email'];
+					$subject = $_POST['subject'];
+					$message = $_POST['message'];
 
-				// Create email	
-				$email_subject = "Message de votre site.";
-				$email_body = "Vous avez recu un nouveaux message \n\n".
-							"Nom: $name \nEmail: $email \nsujet: $subject \nMessage: $message \n";
-				$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
-				$headers .= "From: decap-sablage.com\n";
-				$headers .= "Reply-To: $to";	
-				
-				mail($to, $subject, $message, $email, $headers);
+					echo("<p>name :".$name."</p>");
+					echo("<p>firstName :".$firstName."</p>");
+					echo("<p>email: ".$email."</p>");
+					echo("<p>subject: ".$subject."</p>");
+					echo("<p>message: ".$message."</p>");
 
+					$to = 'parede.yoan.amcs@gmail.com'; // Email submissions are sent to this email
+					echo("reception:".$to);
 
-				if($name==0){
-					echo("bug mail");
-				}else{
-					echo("mail envoyer avc succees");
-				}
-				}*/
-		if(isset($_POST['name']) && ($_POST['firstName']) && ($_POST['email']) && ($_POST['subject']) && ($_POST['message'])) {
-			$destinataire = 'parede.yoan.amcs@gmail.com';
-			// Pour les champs $expediteur / $copie / $destinataire, séparer par une virgule s'il y a plusieurs adresses
-			$expediteur = $_POST['email'];
-			$objet = $_POST['subject']; // Objet du message
-			$headers  = 'MIME-Version: 1.0' . "\n"; // Version MIME
-			$headers .= 'Content-type: text/html; charset=ISO-8859-1'."\n"; // l'en-tete Content-type pour le format HTML
-			$headers .= 'Reply-To: '.$expediteur."\n"; // Mail de reponse
-			$headers .= 'From: "Decap-sablage.fr"<'.$expediteur.'>'."\n"; // Expediteur
-			$headers .= 'Delivered-to: '.$destinataire."\n"; // Destinataire  
-			$message = "Nom: ".$_POST['name']."\nPrenom: ".$_POST["firstName"]."\nEmail: ".$_POST["email"];   
-			$message = $_POST['message'];
-			if (mail($destinataire, $objet, $message, $headers)) // Envoi du message
-			{
-				echo 'Votre message a bien été envoyé ';
-			}
-			else // Non envoyé
-			{
-				echo "Votre message n'a pas pu être envoyé";
-			}
-		}
-		?>
+					// Create email	
+					$email_body = "Vous avez recu un nouveaux message \n\n".
+								"Nom: $name \nEmail: $email \nSujet: $subject \nMessage: $message \n";
+					$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
+					$headers .= "From: decap-sablage.com\n";
+					$headers .= "Reply-To: $to";	
+					
+					mail($to, $subject, $email_body, $headers);
 
+				//}else{
+				//	echo("<p>BUG mail !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</p>");
+				//}
+			?>
+		</div>
 	</body>
 </html>
